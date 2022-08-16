e9a8d1fd1f7e8cc79be78534b4fb8f1134b109f1
rejennylo <rejennylo@gmail.com>
2022-08-16 16:35:40 +0800

    initialize project

---
+++ index.js
@@ -0,0 +1,40 @@
+const Symbols = [
+  'https://assets-lighthouse.alphacamp.co/uploads/image/file/17989/__.png', //黑桃
+  'https://assets-lighthouse.alphacamp.co/uploads/image/file/17992/heart.png', // 愛心
+  'https://assets-lighthouse.alphacamp.co/uploads/image/file/17991/diamonds.png', // 方塊
+  'https://assets-lighthouse.alphacamp.co/uploads/image/file/17988/__.png' // 梅花
+]
+
+const view = {
+  getCardElement(index) {
+    const number = this.teansformNumber((index % 13) + 1)
+    const symbol = Symbols[Math.floor(index / 13)]
+    return `<div class="card">
+      <p>${number}</p>
+      <img src="${symbol}" alt="">
+      <p>${number}</p>
+    </div>`
+  },
+
+  teansformNumber(number) {
+    switch(number) {
+      case 1:
+        return 'A'
+      case 11:
+        return 'J'
+      case 12:
+        return 'Q'
+      case 13:
+        return 'K'
+      default:
+        return number
+    }
+  },
+
+  displayCards() {
+    const rootElement = document.querySelector('#cards')
+    rootElement.innerHTML = Array.from(Array(52).keys()).map(index => this.getCardElement(index)).join('')
+  }
+}
+
+view.displayCards()
